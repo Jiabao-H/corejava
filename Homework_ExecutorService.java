7bca4ab0a19828814038166a72774c5cc0f04e92
Jiabao-H <70572553+Jiabao-H@users.noreply.github.com>
2022-09-30 14:09:18 -0700

    Update Homework_ExecutorService.java

--- Homework_ExecutorService.java
+++ Homework_ExecutorService.java
@@ -1,20 +1,10 @@
-package com.interview.coding.course;
-
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-/**
- * 1)	Define a inner class A has method “getMethod()”
- * which return string “A.getMethod” and another inner class B has method
- * “getMethod()” which return string “B.getMethod”.
- * write another method “runSameTime()”
- * which should let A and B getMethod to run at the same time,
- * but the “runSameTime()”
- * method should return a string “B.getMethod A.getMethod”
- */
-public class Homework_ExecutorService {
+
+public class ExecutorService {
 
     public class A{
         public String getMethod(){
@@ -28,13 +18,14 @@ public class Homework_ExecutorService {
         }
     }
 
+   //need to be excutorService
     public void runSameTime(){
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         try{
-            Future futureA = executorService.submit(()-> new A().getMethod());
-            Future futureB = executorService.submit(()-> new B().getMethod());
+            Future futureA = executorService.submit(()-> new A().getMethod()); // futureA 
+            Future futureB = executorService.submit(()-> new B().getMethod());// futureB
 
-            boolean notAllDone = true;
+            //boolean notAllDone = true;
             while(notAllDone){
                 if(futureA.isDone() && futureB.isDone()) {
                     notAllDone = false;
